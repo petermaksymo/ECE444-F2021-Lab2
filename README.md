bbe6b473283f97bc29ec43cc4b0689d33170dc4e
petermaksymo <peter.maksymowsky@mail.utoronto.ca>
2021-09-26 21:38:51 -0400

    Activity 4.2 complete with email validation

--- README.md
+++ README.md
@@ -12,3 +12,17 @@
 # Activity 3:
 
 ![screenshot of activity 3](./images/Activity3.png)
+
+# Activity 4:
+
+## Activity 4.2:
+![screenshot of activity 4.2](./images/Activity4.2.png)
+
+## Activity 4.3:
+![screenshot of activity 4.3](./images/Activity4.3.png)
+
+## Activity 4.4:
+![screenshot of activity 4.4](./images/Activity4.4.png)
+
+## Activity 4.5:
+![screenshot of activity 4.5](./images/Activity4.5.png)
--- hello.py
+++ hello.py
@@ -3,7 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret string that will never be cracked or pushed to a public github repo'
@@ -14,6 +15,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,14 +23,21 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
 
+        session['email_valid'] = 'utoronto' in form.email.data
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html',
                            form=form,
-                           name=session.get('name'))
+                           name=session.get('name'),
+                           email=session.get('email'),
+                           email_valid=session.get('email_valid'))
 
 @app.route('/user/<name>')
 def user(name):
